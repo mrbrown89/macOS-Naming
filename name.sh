4b31015ab33ce5b82a36573e1c458ec50a56de17
Matt <mattb@nsccreative.com>
2024-05-06 09:44:04 +0100

    Started on the main function

--- name.sh
+++ name.sh
@@ -2,4 +2,13 @@
 
 SERIAL_NUMBER=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
 DEPT=it
-echo nsc-mac-$DEPT-$SERIAL_NUMBER
+
+NEWNAME=nsc-mac-$DEPT-$SERIAL_NUMBER
+
+MAIN() {
+    scutil --set ComputerName "$NEWNAME"
+    scutil --set HostName "$NEWNAME"
+    scutil --set LocalHostName "$NEWNAME"
+}
+
+

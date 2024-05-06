5c4155ce92d33834c07d578208d678bdfdf1319b
Matt <mattb@nsccreative.com>
2024-05-06 10:00:54 +0100

    First draft of the script done

--- name.sh
+++ name.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-SERIAL_NUMBER=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
-DEPT=it
+SERIALNUMBER=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
+DEPT="$3"
 
-NEWNAME=nsc-mac-$DEPT-$SERIAL_NUMBER
+NEWNAME="nsc-mac-$DEPT-$SERIALNUMBER"
 
 MAIN() {
     scutil --set ComputerName "$NEWNAME"
     scutil --set HostName "$NEWNAME"
     scutil --set LocalHostName "$NEWNAME"
+    jamf recon
 }
 
-
+MAIN
